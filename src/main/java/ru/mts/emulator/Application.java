b5f571a4fea97f4d10bcb64a15b6b96d553abc30
Aleksey Kolpakov <kolpak.al6@yandex.ru>
2022-03-20 14:13:52 +0300

    simple app for send mq message

--- src/main/java/ru/mts/emulator/Application.java
+++ src/main/java/ru/mts/emulator/Application.java
@@ -1,11 +1,33 @@
 package ru.mts.emulator;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
+@EnableJms
 public class Application {
+
+    static final String qName = "DEV.QUEUE.1"; // A queue from the default MQ Developer container config
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+
+        // Launch the application
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // Create the JMS Template object to control connections and sessions.
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+        // Send a single message with a timestamp
+        String msg = "Hello from IBM MQ at " + new Date();
+
+        // The default SimpleMessageConverter class will be called and turn a String
+        // into a JMS TextMessage
+        jmsTemplate.convertAndSend(qName, msg);
+
     }
 }
